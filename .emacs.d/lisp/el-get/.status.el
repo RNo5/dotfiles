41657db6dd9935d49bd18e4dc4fd2705fd78d5a7
RNo5 <numayama2009@gmail.com>
2016-03-16 01:50:08 -0600

    remove .gitignore from .emacs.d/lisp/el-get

---
+++ .emacs.d/lisp/el-get/.status.el
@@ -0,0 +1,58 @@
+((auto-complete status "installed" recipe
+		(:name auto-complete :after nil :features
+		       (auto-complete-config)
+		       :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+		       (popup fuzzy)
+		       :post-init
+		       (progn
+			 (add-to-list 'ac-dictionary-directories
+				      (expand-file-name "dict" default-directory))
+			 (ac-config-default))))
+ (c-eldoc status "installed" recipe
+	  (:name c-eldoc :after nil :description "eldoc-mode plugin for C source code" :type github :pkgname "nflath/c-eldoc" :post-init
+		 (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)))
+ (cedet status "required" recipe nil)
+ (cl-lib status "required" recipe nil)
+ (dash status "installed" recipe
+       (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
+ (el-get status "required")
+ (epl status "installed" recipe
+      (:name epl :description "EPL provides a convenient high-level API for various package.el versions, and aims to overcome its most striking idiocies." :type github :pkgname "cask/epl"))
+ (fuzzy status "installed" recipe
+	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (ggtags status "installed" recipe
+	 (:name ggtags :after nil :description "Use GNU Global in Emacs." :type github :pkgname "leoliu/ggtags"))
+ (init-loader status "installed" recipe
+	      (:name init-loader :after nil :website "https://github.com/emacs-jp/init-loader" :description "It enables you to categorize your configurations and separate them into multiple files." :type github :pkgname "emacs-jp/init-loader"))
+ (let-alist status "required" recipe nil)
+ (package status "installed" recipe
+	  (:name package :description "ELPA implementation (\"package.el\") from Emacs 24" :builtin "24" :type http :url "http://repo.or.cz/w/emacs.git/blob_plain/ba08b24186711eaeb3748f3d1f23e2c2d9ed0d09:/lisp/emacs-lisp/package.el" :shallow nil :features package :post-init
+		 (progn
+		   (let
+		       ((old-package-user-dir
+			 (expand-file-name
+			  (convert-standard-filename
+			   (concat
+			    (file-name-as-directory default-directory)
+			    "elpa")))))
+		     (when
+			 (file-directory-p old-package-user-dir)
+		       (add-to-list 'package-directory-list old-package-user-dir)))
+		   (setq package-archives
+			 (bound-and-true-p package-archives))
+		   (mapc
+		    (lambda
+		      (pa)
+		      (add-to-list 'package-archives pa 'append))
+		    '(("ELPA" . "http://tromey.com/elpa/")
+		      ("melpa" . "http://melpa.org/packages/")
+		      ("gnu" . "http://elpa.gnu.org/packages/")
+		      ("marmalade" . "http://marmalade-repo.org/packages/")
+		      ("SC" . "http://joseito.republika.pl/sunrise-commander/"))))))
+ (pkg-info status "installed" recipe
+	   (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends
+		  (dash epl)))
+ (popup status "installed" recipe
+	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :depends cl-lib :pkgname "auto-complete/popup-el"))
+ (seq status "installed" recipe
+      (:name seq :description "Sequence manipulation library for Emacs" :builtin "25" :type github :pkgname "NicolasPetton/seq.el")))
