2a7159e6f3f64e04b1f15027d614b27dd9cf1c51
RNo5 <numayama2009@gmail.com>
2018-02-06 23:57:44 -0500

    show ip for CentOS7

--- bin/whatismyip.sh
+++ bin/whatismyip.sh
@@ -5,8 +5,12 @@
 
 if [ $(uname | grep -i "cygwin") ]; then # for Cygwin
     ipconfig |grep -a 'IPv4' | cut -d: -f2 | awk '{ print $1}'
-else # for Linux
+elif [ $(/sbin/ifconfig |grep -i "eth0") ]; then # for Linux openSUSE (eth0) 
     /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+elif [ !$(/sbin/ifconfig | grep -qi "eno1") ]; then # for Linux CentOS (eno1)
+    /sbin/ifconfig eno1 | grep 'inet '  | awk '{ print $2}'
+else # for unknown interface
+    echo "unknown interface"
 fi
 
 
