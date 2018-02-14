779f62e955495df747ef7554fb9156e33b92cc00
RNo5 <numayama2009@gmail.com>
2018-02-14 17:45:16 +0900

    show ip for openSUSE (eth0)

--- bin/whatismyip.sh
+++ bin/whatismyip.sh
@@ -5,7 +5,8 @@
 
 if [ $(uname | grep -i "cygwin") ]; then # for Cygwin
     ipconfig |grep -a 'IPv4' | cut -d: -f2 | awk '{ print $1}'
-elif [ $(/sbin/ifconfig |grep -i "eth0") ]; then # for Linux openSUSE (eth0) 
+#elif [ 1 ]; then # for Linux openSUSE (eth0) 
+elif [ !$(/sbin/ifconfig |grep -qi "eth0") ]; then # for Linux openSUSE (eth0)
     /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 elif [ !$(/sbin/ifconfig | grep -qi "eno1") ]; then # for Linux CentOS (eno1)
     /sbin/ifconfig eno1 | grep 'inet '  | awk '{ print $2}'
