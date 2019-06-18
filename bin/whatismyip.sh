06fb8ad381ea207919cffa0b909f599ccd0ad7b2
RNo5 <numayama2009@gmail.com>
2019-06-18 18:42:20 +0900

    use ip but not ifconfig command in whatismyip.sh

--- bin/whatismyip.sh
+++ bin/whatismyip.sh
@@ -6,6 +6,8 @@
 if [ $(uname | grep -i "cygwin") ]; then # for Cygwin
     ipconfig |grep -a 'IPv4' | cut -d: -f2 | awk '{ print $1}'
 #elif [ 1 ]; then # for Linux openSUSE (eth0) 
+elif [ !$(/bin/ip a s|grep -qi "eth0") ]; then # for Linux openSUSE (eth0)
+    /bin/ip a s dev eth0 | grep 'inet ' | awk '{ print $2}' | cut -d'/' -f1
 elif [ !$(/sbin/ifconfig |grep -qi "eth0") ]; then # for Linux openSUSE (eth0)
     /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 elif [ !$(/sbin/ifconfig | grep -qi "eno1") ]; then # for Linux CentOS (eno1)
