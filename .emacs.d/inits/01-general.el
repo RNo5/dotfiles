1549023db621ff223555af2d6486d6df4db913c4
RNo5 <numayama2009@gmail.com>
2021-01-22 10:14:57 +0900

    add revert-buffer in .emacs.d

--- .emacs.d/inits/01-general.el
+++ .emacs.d/inits/01-general.el
@@ -1,2 +1,11 @@
 ;;; symlinkは必ず追いかける
 (setq vc-follow-symlinks t)
+
+
+
+;;; revert buffer
+(defun revert-buffer-no-confirm ()
+  "Revert buffer without confirmation."
+  (interactive) (revert-buffer t t))
+
+(global-set-key (kbd "C-c C-l") 'revert-buffer-no-confirm)
